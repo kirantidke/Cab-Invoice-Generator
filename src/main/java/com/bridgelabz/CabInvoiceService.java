a7bcf8eb36924a627453cc2fd9e39a0ee3da6d6c
kirantidke <kirantidke2013@gmail.com>
2021-07-15 21:54:59 +0530

    [Kiran]ADDED:program to cab invoice generator

---
+++ src/main/java/com/bridgelabz/CabInvoiceService.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class CabInvoiceService {
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to cab invoice generator project");
+		
+	}
+	
+}
