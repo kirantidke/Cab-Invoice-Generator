05c5f8e61ee610d55be79cdf71e9ac092631360d
kirantidke <kirantidke2013@gmail.com>
2021-07-16 00:01:21 +0530

    [Kiran]ADDED:code for multiple rides

--- src/test/java/com/bridgelabz/CabInvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceServiceTest.java
@@ -8,22 +8,30 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class CabInvoiceServiceTest 
-{
-	 CabInvoiceService cabInvoiceService = new CabInvoiceService();
+public class CabInvoiceServiceTest {
+	CabInvoiceService cabInvoiceService = new CabInvoiceService();
 
-	    @Test
-	    public void givenDistanceAndTime_shouldReturn_totalFare(){
-	        double  distance = 5.0;
-	        int time = 15;
-	        double fare = cabInvoiceService.calculatorFare(distance, time);
-	        Assert.assertEquals(65,fare,0.0);
-	    }
-	    @Test
-	    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
-	        double distance = 0.1;
-	        int time = 2;
-	        double fare = cabInvoiceService.calculatorFare(distance, time);
-	        Assert.assertEquals(5, fare,0.0);
-	    }
+	@Test
+	public void givenDistanceAndTime_shouldReturn_totalFare() {
+		double distance = 5.0;
+		int time = 15;
+		double fare = cabInvoiceService.calculatorFare(distance, time);
+		Assert.assertEquals(65, fare, 0.0);
+	}
+
+	@Test
+	public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+		double distance = 0.1;
+		int time = 2;
+		double fare = cabInvoiceService.calculatorFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
+	}
+
+	@Test
+	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1), new Ride(4.1, 25) };
+		double totalFare = cabInvoiceService.calculateFareForMultipleRides(rides);
+		Assert.assertEquals(96, totalFare, 0);
+	}
+
 }
